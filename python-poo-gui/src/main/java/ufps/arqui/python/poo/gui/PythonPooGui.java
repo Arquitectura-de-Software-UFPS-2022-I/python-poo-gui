64c9e94aab228b25f360b4b946ef3929950df10c
OMAR RAMON <omarmontes.879@gmail.com>
2022-05-31 18:58:59 -0500

    Creación de proyecto

---
+++ python-poo-gui/src/main/java/ufps/arqui/python/poo/gui/PythonPooGui.java
@@ -0,0 +1,13 @@
+package ufps.arqui.python.poo.gui;
+
+/**
+ * Clase Main para la inicialización del proyecto.
+ * @author Omar Ramón Montes
+ */
+public class PythonPooGui {
+    
+    public static void main(String[] args) {
+        
+    }
+    
+}
