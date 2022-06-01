00a93735c40a9f0b924b97cde0ff0743ef39b546
OMAR RAMON <omarmontes.879@gmail.com>
2022-05-31 19:21:13 -0500

    Mover proyecto

---
+++ src/main/java/ufps/arqui/python/poo/gui/views/IMainView.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ufps.arqui.python.poo.gui.views;
+
+import java.util.Observable;
+import java.util.Observer;
+
+/**
+ *
+ * @author USUARIO
+ */
+public interface IMainView extends Observer {
+    
+}
---
+++ src/main/java/ufps/arqui/python/poo/gui/views/impl/MainView.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ufps.arqui.python.poo.gui.views.impl;
+
+import java.util.Observable;
+import ufps.arqui.python.poo.gui.views.IMainView;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class MainView implements IMainView{
+
+    @Override
+    public void update(Observable o, Object arg) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
