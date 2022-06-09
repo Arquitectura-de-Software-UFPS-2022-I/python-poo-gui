dbca874b53f9f4f6f597a9e131fa65bbf7b30434
Freddy Chia <elipson39z@gmail.com>
2022-06-09 10:22:51 -0500

    requerimiento 1 back

--- src/main/java/ufps/arqui/python/poo/gui/controllers/impl/ProyectoController.java
+++ src/main/java/ufps/arqui/python/poo/gui/controllers/impl/ProyectoController.java
@@ -1,5 +1,10 @@
 package ufps.arqui.python.poo.gui.controllers.impl;
 
+import java.io.File;
+
+import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
+
 import ufps.arqui.python.poo.gui.controllers.IProyectoController;
 import ufps.arqui.python.poo.gui.controllers.ITerminalController;
 import ufps.arqui.python.poo.gui.models.Proyecto;
@@ -8,14 +13,47 @@ import ufps.arqui.python.poo.gui.models.Proyecto;
  * Implementación dl Controlador del mundo del proyecto.
  *
  * Donde se administra las clases creadas por el usuario.
+ * 
  * @author Omar Ramón Montes
  */
 public class ProyectoController implements IProyectoController {
 
-    private final Proyecto proyecto;
+	//private final Proyecto proyecto;
 
-    public ProyectoController(Proyecto proyecto) {
-        this.proyecto = proyecto;
-    }
+     public ProyectoController() {
+		
+		
+	}
+	public ProyectoController(Proyecto proyecto) {
+		//this.proyecto = proyecto;
+	}
+	
 
+	public static void main(String[] args) {
+
+		Proyecto proyectico = new Proyecto();
+		proyectico.setNombre("pruebaPython");
+		proyectico.setDirectorio("C:\\Users\\FREDDY\\Desktop");
+		
+		ProyectoController project = new ProyectoController();
+		project.crearProyecto(proyectico);
+		
+
+	}
+
+	public boolean crearProyecto(Proyecto proyecto) {
+		
+		String directorios = proyecto.getDirectorio() +"/" + proyecto.getNombre()+"/src/main/python";
+
+		if (new File(directorios).mkdirs()) {
+			System.out.println("directorios creados");
+			return true;
+		}
+		else {
+			System.out.println("directorio ya existe");
+			return false;
+		}
+
+	}
+  
 }
