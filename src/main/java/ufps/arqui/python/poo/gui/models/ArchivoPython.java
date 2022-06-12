c41246b9ca06ae5bc98f8ba393f2932edce6681d
Jose Pineda Peña <joseanibalpp@ufps.edu.co>
2022-06-11 21:13:59 -0500

    Crear clases

--- src/main/java/ufps/arqui/python/poo/gui/models/ArchivoPython.java
+++ src/main/java/ufps/arqui/python/poo/gui/models/ArchivoPython.java
@@ -1,24 +1,38 @@
 package ufps.arqui.python.poo.gui.models;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Modelado de archivo de python.
- * 
+ *
  * @author Omar Ramón Montes
  */
 public class ArchivoPython {
-   
+
     /**
      * Representación del archivo python.
      */
     private File archivo;
-    
+
     /**
      * Listado de clases que contiene el archivo.
      */
-    private List<ClasePython> clases;
+    private List<ClasePython> clases = new ArrayList<ClasePython>();
+
+    /**
+     * Verifica si la clase que se quiere agregar ya existe
+     */
+    public boolean claseExiste(String nombreClase) {
+        boolean respuesta = false;
+        for (int i = 0; i < this.clases.size(); i++) {
+            if (this.clases.get(i).getNombre().equals(nombreClase)) {
+                respuesta = true;
+            }
+        }
+        return respuesta;
+    }
 
     public File getArchivo() {
         return archivo;
@@ -39,6 +53,5 @@ public class ArchivoPython {
     public void addClase(ClasePython clase) {
         this.clases.add(clase);
     }
-    
-    
+
 }
